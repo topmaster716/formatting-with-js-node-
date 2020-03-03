a01071fdf4285da0aa8bf99dba58f67c02492a9d
topmaster716 <59236904+topmaster716@users.noreply.github.com>
2020-03-03 13:25:18 -0600

    multi item: fixed

--- formating.js
+++ formating.js
@@ -15,9 +15,9 @@ rl.on('line', function(line) {
     // check new item
     if(line.indexOf("Info 1:") == 0 && item_no > 0){
         // new item
+        console.log("comment1");
         writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` + 
-        `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}`, 'utf-8');
-        writeStream.end();
+        `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}\n`, 'utf-8');
         infoItem = ["","","","","","","","","","","","",""];
     }
     if(line.indexOf("Info") == 0){
@@ -26,12 +26,16 @@ rl.on('line', function(line) {
         itemStr = itemStr.replace(/\s/g,"");
         infoItem[infoIndx - 1] = itemStr.replace("/",":");
         if (infoIndx == 8) item_no ++;
+        console.log(infoIndx);
     }
 });
 
 // end
 rl.on('close', function(line) {
     // console.log('Total lines : ' + line_no);
+    console.log("end");
+    for(let i=0; i < 13; i ++)
+        console.log(infoItem[i]);
     writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
     `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}`, 'utf-8');
     writeStream.end();
