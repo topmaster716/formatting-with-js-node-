8e1d2b2376cf3f1998ed053f45daf4ec3c582403
topmaster716 <59236904+topmaster716@users.noreply.github.com>
2020-03-03 14:10:34 -0600

    comment removed

--- formating.js
+++ formating.js
@@ -8,44 +8,38 @@ let writeStream = fs.createWriteStream('result.csv');
 
 let item_no = 0;
 
-let infoItem = [""];
-rl.on('line', function(line) {
+let infoItem = [];
+rl.on('line', function (line) {
     // check new item
-    if(line.indexOf("Info 1:") == 0 && item_no > 0){
+    if (line.indexOf("Info 1:") == 0 && item_no > 0) {
         // new item
-        console.log("comment1");
-        if(infoItem[8] != "")
-            writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` + 
-            `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}\n`, 'utf-8');
+        if (infoItem[8] != "")
+            writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
+                `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}\n`, 'utf-8');
         else
-            writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` + 
-            `${infoItem[3]}:CANADA:${infoItem[0]}\n`, 'utf-8');
-        infoItem = ["","","","","","","","","","","","",""];
+            writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
+                `${infoItem[3]}:CANADA:${infoItem[0]}\n`, 'utf-8');
+        infoItem = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
     }
-    if(line.indexOf("Info") == 0){
+    if (line.indexOf("Info") == 0) {
         let infoIndx = line.slice(5, line.indexOf(":"));
         let itemStr = line.slice(line.indexOf(":") + 2, line.length);
-        itemStr = itemStr.replace(/\s/g,"");
-        infoItem[infoIndx - 1] = itemStr.replace("/",":");
-        if (infoIndx == 8) item_no ++;
-        console.log(infoIndx);
+        itemStr = itemStr.replace(/\s/g, "");
+        infoItem[infoIndx - 1] = itemStr.replace("/", ":");
+        if (infoIndx == 8) item_no++;
     }
 });
 
 // end
-rl.on('close', function(line) {
-    console.log("end");
-    for(let i=0; i < 13; i ++)
-        console.log(infoItem[i]);
-    if(infoItem[8] != "")
-        writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` + 
-        `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}`, 'utf-8');
+rl.on('close', function (line) {
+    if (infoItem[8] != "")
+        writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
+            `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}`, 'utf-8');
     else
-        writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` + 
-        `${infoItem[3]}:CANADA:${infoItem[0]}`, 'utf-8');
+        writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
+            `${infoItem[3]}:CANADA:${infoItem[0]}`, 'utf-8');
 });
 
 writeStream.on('finish', () => {
 
 });
-
