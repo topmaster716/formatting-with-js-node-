9b467bcd80be21f043bafaa92acdbc7ac80b8925
topmaster716 <59236904+topmaster716@users.noreply.github.com>
2020-03-03 19:42:14 -0600

    User can set Information key

--- formating.js
+++ formating.js
@@ -1,43 +1,69 @@
 const readline = require('readline');
 const fs = require('fs');
+const infoStr = [
+    "Info 1",
+    "Info 2",
+    "Info 3",
+    "Info 4",
+    "Info 5",
+    "Info 6",
+    "Info 7",
+    "Info 8",
+    "Info 9",
+    "Info 10",
+    "Info 11",
+    "Info 12",
+    "Info 13"
+];
 
 let rl = readline.createInterface({
     input: fs.createReadStream('task.csv')
 });
 let writeStream = fs.createWriteStream('result.csv');
 
-let item_no = 0;
+let itemCount = 0;
+//let itemIndex = 0;
 
-let infoItem = [];
+let V = [];
 rl.on('line', function (line) {
     // check new item
-    if (line.indexOf("Info 1:") == 0 && item_no > 0) {
+    if (infoStr.indexOf(line.slice(0, line.indexOf(":"))) == 0 && itemCount > 0) {
         // new item
-        if (infoItem[8] != "")
-            writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
-                `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}\n`, 'utf-8');
+        console.log(`first comm + ${line}`);
+        if (V[8] != "")
+            writeStream.write(`${V[5]}:${V[6]}:${V[7]}:${V[2]} ` +
+                `${V[3]}:${V[8]}:${V[11]}:${V[10]}:CANADA:${V[12]}:${V[0]}\n`, 'utf-8');
         else
-            writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
-                `${infoItem[3]}:CANADA:${infoItem[0]}\n`, 'utf-8');
-        infoItem = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
+            writeStream.write(`${V[5]}:${V[6]}:${V[7]}:${V[2]} ` +
+                `${V[3]}:CANADA:${V[0]}\n`, 'utf-8');
+        V = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
+        itemIndex = 0;
     }
-    if (line.indexOf("Info") == 0) {
-        let infoIndx = line.slice(5, line.indexOf(":"));
-        let itemStr = line.slice(line.indexOf(":") + 2, line.length);
-        itemStr = itemStr.replace(/\s/g, "");
-        infoItem[infoIndx - 1] = itemStr.replace("/", ":");
-        if (infoIndx == 8) item_no++;
+    // check itemIndex
+    if (line.indexOf(":") > 0) {
+
+        let infoIndx = infoStr.indexOf(line.slice(0, line.indexOf(":"))); // ().indexOf(infoStr);
+
+        console.log(`index = ${infoIndx}`);
+        if (infoIndx >= 0) {
+
+            let itemStr = line.slice(line.indexOf(":") + 2, line.length);
+            itemStr = itemStr.replace(/\s/g, "");
+            V[infoIndx] = itemStr.replace("/", ":");
+            console.log(V[infoIndx]);
+            if (infoIndx == 7) itemCount++;
+        }
     }
 });
 
 // end
 rl.on('close', function (line) {
-    if (infoItem[8] != "")
-        writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
-            `${infoItem[3]}:${infoItem[8]}:${infoItem[11]}:${infoItem[10]}:CANADA:${infoItem[12]}:${infoItem[0]}`, 'utf-8');
+    if (V[8] != "")
+        writeStream.write(`${V[5]}:${V[6]}:${V[7]}:${V[2]} ` +
+            `${V[3]}:${V[8]}:${V[11]}:${V[10]}:CANADA:${V[12]}:${V[0]}`, 'utf-8');
     else
-        writeStream.write(`${infoItem[5]}:${infoItem[6]}:${infoItem[7]}:${infoItem[2]} ` +
-            `${infoItem[3]}:CANADA:${infoItem[0]}`, 'utf-8');
+        writeStream.write(`${V[5]}:${V[6]}:${V[7]}:${V[2]} ` +
+            `${V[3]}:CANADA:${V[0]}`, 'utf-8');
 });
 
 writeStream.on('finish', () => {
